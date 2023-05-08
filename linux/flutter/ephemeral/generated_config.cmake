ac0d577c83d5674b7fc8c91fa565863d81a7849f
ajaysathvik <111119810+ajaysathvik@users.noreply.github.com>
2023-05-08 16:47:27 +0530

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ajay/Downloads/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ajay/chatapp_firebase/chatapp_firebase" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ajay/Downloads/flutter"
+  "PROJECT_DIR=/home/ajay/chatapp_firebase/chatapp_firebase"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ajay/chatapp_firebase/chatapp_firebase/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ajay/chatapp_firebase/chatapp_firebase/lib/main.dart"
+)
